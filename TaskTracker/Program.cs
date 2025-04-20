ca2e0804269f4a592c29dfadfcc8dee54c712915
gabriel-g4 <gbgarcia642@gmail.com>
2025-04-20 12:23:54 -0300

    init

---
+++ TaskTracker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TaskTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Escribi ");
+            string result = Console.ReadLine();
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
